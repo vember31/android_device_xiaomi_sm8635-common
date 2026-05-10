d28dee483f62b2752793a0a276b599ac0179aa3f
vember31 <prestigioustennis@gmail.com>
2026-05-10 13:59:26 -0500

    sm8635-common: Add modem firmware mount

--- common.mk
+++ common.mk
@@ -231,7 +231,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
     vendor_dsp_mountpoint \
-    vendor_firmware_mnt_mountpoint
+    vendor_firmware_mnt_mountpoint \
+    vendor_modem_firmware_mountpoint
 
 # Parts
 PRODUCT_PACKAGES += \
