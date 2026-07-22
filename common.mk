6c05f9cee88af8bba541ccfc1625b893f5bac153
vember31 <pretigioustennis@gmail.com>
2026-07-22 00:00:42 +0000

    sm8635-common: Remove DolbyAtmos package

--- common.mk
+++ common.mk
@@ -116,10 +116,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer3-V2-ndk.vendor \
     vendor.qti.hardware.display.composer3-V1-ndk.vendor
 
-# Dolby
-PRODUCT_PACKAGES += \
-    DolbyAtmos
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
